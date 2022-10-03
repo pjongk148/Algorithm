62e97619d455fe101c60ddce81e16229ba7b01ee
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2022-10-03 22:04:34 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,22 +1,7 @@
 def solution(citations):
-    n = len(citations)
-    dic = {}
-
-    for v in citations:
-        if dic.get(v): dic[v] +=1
-        else: dic[v] = 1
-
-    arr = list(set(citations))
-
-    max = 0
-    cur = 0 #현재 인용수
-    for i in range(arr[-1]+1): 
-        if i in arr: 
-            if n - cur >= i and cur <= i:
-                max = i
-            cur += dic[i]
-
-        else:
-            if n - cur >= i and cur <= i:
-                max = i
-    return max
+    citations = sorted(citations)
+    l = len(citations)
+    for i in range(l):
+        if citations[i] >= l-i:
+            return l-i
+    return 0
