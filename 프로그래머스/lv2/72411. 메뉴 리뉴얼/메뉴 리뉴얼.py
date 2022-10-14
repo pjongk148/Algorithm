a3fc793f154fdb81bb28dce3af6f06ffb761e542
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2022-10-14 21:52:08 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 1.32 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,30 +1,21 @@
 import itertools
 from collections import Counter
+
 def solution(orders, course):
-    
-    arr = []
-    ans = []
-    for i in orders:
-        for num in course:
-            nPr = list(itertools.combinations(sorted(i),num))
-            arr += nPr
-
-    combi = Counter(arr).most_common()
-
-    for num in course:
-        max = 2
-        for i in combi:
-            if len(i[0]) == num:
-                if i[1] >= max:
-                    ans.append("".join(i[0]))
-                    max = i[1]
-                else:
-                    break
-            else:
-                continue
+    from itertools import combinations
+    from collections import Counter
 
 
 
-    ans.sort()
+    answer = []
+    for i in course:
+        temp = []
+        for menu in orders:
+            comb = combinations(sorted(menu), i)
+            temp += comb
+        counter = Counter(temp)
+        if len(counter) != 0 and max(counter.values()) > 1:
+            answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]
 
-    return ans
+    answer.sort()
+    return answer
