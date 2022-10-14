6c0de43a47c83ac2297bb274587c833fa177fe52
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2022-10-14 21:55:11 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.29 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,21 +1,35 @@
 import itertools
 from collections import Counter
-
+"""
+조건:
+1. 가장 많이 "함께" 주문된 단품메뉴의 "조합"
+2. 최소 두 가지 이상 + 최소 2명 이상 주문되어야 함
+각 손님은 메뉴 2개 이상 주문
+"""
 def solution(orders, course):
-    from itertools import combinations
-    from collections import Counter
+    arr = []
+    ans = []
+    for i in orders:
+        for num in course:
+            #조건 1 만족하기위한 조합(sorted(i)를 통해 문자열순서 통일)
+            nPr = list(itertools.combinations(sorted(i),num))
+            arr += nPr
+
+    combi = Counter(arr).most_common()
+
+    for num in course: #요구하는 세트메뉴 개수
+        max = 2 #최소 두가지 이상 주문
+        for i in combi:
+            if len(i[0]) == num:
+                if i[1] >= max:
+                    ans.append("".join(i[0]))
+                    max = i[1]
+                else:
+                    break
+            else:
+                continue
 
 
 
-    answer = []
-    for i in course:
-        temp = []
-        for menu in orders:
-            comb = combinations(sorted(menu), i)
-            temp += comb
-        counter = Counter(temp)
-        if len(counter) != 0 and max(counter.values()) > 1:
-            answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]
-
-    answer.sort()
-    return answer
+    ans.sort()
+    return ans
