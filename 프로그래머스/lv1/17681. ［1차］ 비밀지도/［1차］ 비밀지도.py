9abdcf4ce1370c4d4c8018f67792279e190399e1
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2022-10-18 23:32:41 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -4,13 +4,15 @@ def solution(n, arr1, arr2):
     dic[0] = " "
 
     ans = []
-
+    #n번 만큼 순회하면서 2진수 형태로 arr요소를 변경
     for i in range(n):
         tmp1 = bin(arr1[i])[2:]
         tmp2 = bin(arr2[i])[2:]
+        #부족한 길이만큼 0을 채워준다
         tmp1 = "0" * (n- len(tmp1)) + tmp1
         tmp2 = "0" * (n- len(tmp2)) + tmp2
         st = ""
+        #반복문을 한번 더 돌면서 1 or 0 =0을 이용하여 str생성 후 ans에 담아준 뒤 반환
         for i in range(n):
             st += dic[int(tmp1[i]) or int(tmp2[i])]
         ans.append(st)
