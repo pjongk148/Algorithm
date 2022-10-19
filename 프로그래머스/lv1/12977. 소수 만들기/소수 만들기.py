294b2e7a9e502c06c1b01f379a68e2c57b5a2346
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2022-10-19 14:16:19 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,25 +1,14 @@
-import itertools
+from itertools import combinations
 import math
 def solution(nums):
-    arr = itertools.combinations(nums, 3)
-
-    ans = list(map(sum,arr))
-    cnt = 0
-    for i in ans:
-        print("i = " + str(i))
+    arr = list(map(sum,combinations(nums, 3)))
+    cnt = len(arr)
+    for i in arr:
         if i % 2 == 0:
-            continue
+            cnt -=1
         else:
-            tmp = True
             for j in range(3,int(math.sqrt(i))+1,2):
                 if i % j == 0:
-                    tmp = False
+                    cnt -=1
                     break
-            if tmp:
-                print(i)
-                cnt +=1
-
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
     return cnt
