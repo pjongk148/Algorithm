75f82d4e730640f9790eaf5974a6158889dcea54
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2022-10-19 09:33:07 +0900

    [level 1] Title: 모의고사, Time: 3.16 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,22 +1,19 @@
 def solution(answers):
-    ans_1 = [1,2,3,4,5]
-    ans_2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    ans_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    count = [0,0,0]
-    ans_1 = ans_1 * (len(answers) // 5) + ans_1[:len(answers) % 5]
-    ans_2 = ans_2 * (len(answers) // 5) + ans_2[:len(answers) % 5]
-    ans_3 = ans_3 * (len(answers) // 5) + ans_3[:len(answers) % 5]
+    num1 = [1,2,3,4,5]
+    num2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    num3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    cnt = [0,0,0]
+    ans =[]
+    for idx, answer in enumerate(answers):
+        if answer == num1[idx%len(num1)]:
+            cnt[0] += 1
+        if answer == num2[idx%len(num2)]:
+            cnt[1] += 1
+        if answer == num3[idx%len(num3)]:
+            cnt[2] += 1
 
-    ans = []
-    for i in range(len(answers)):
-        if ans_1[i] == answers[i]:
-            count[0]+=1
-        if ans_2[i] == answers[i]:
-            count[1]+=1
-        if ans_3[i] == answers[i]:
-            count[2]+=1
+    for idx, s in enumerate(cnt):
+        if s == max(cnt):
+            ans.append(idx+1)
 
-    for i in range(len(count)):
-        if count[i] == max(count):
-            ans.append(i+1)
     return ans
