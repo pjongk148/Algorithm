0ffe4aad8c656020a60c2f6cd0bb7c7f5ae8da0b
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2022-10-21 23:34:40 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,13 +1,9 @@
 def solution(left, right):
     ans = 0
     for i in range(left,right+1):
-        tmp = 0
-        for j in range(1,i+1):
-            if i % j == 0:
-                tmp +=1
-        if tmp % 2 == 0:
-            ans += i
-        else:
+        if int(i**(0.5)) == i**(0.5):
             ans -= i
+        else:
+            ans += i
     
     return ans
