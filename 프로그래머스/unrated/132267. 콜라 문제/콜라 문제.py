fb1329c976243a0015dacc12b56eb41006b059a4
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2022-10-25 15:34:55 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -1,10 +1,6 @@
 def solution(a, b, n):
     i = 0
     while (n >= a):
-        print(i, n)
-        i += bottle(a,b,n)[0]
-        n = bottle(a,b,n)[0] + bottle(a,b,n)[1]
+        i += b*(n//a)
+        n = b*(n//a)+ n%a
     return i
-
-def bottle(a,b,n):
-    return [b*(n//a),n%a]
