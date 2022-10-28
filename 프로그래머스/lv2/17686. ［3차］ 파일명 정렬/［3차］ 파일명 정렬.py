f6beba3f12b2d40f6ada3140bf4d4273a502528d
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2022-10-28 23:35:58 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 135.82 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,8 +1,31 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
 def solution(files):
     tmp = []
     head , number, tail= "","",""
     for file in files:
         for i in range(len(file)):
+            # 1. 첫번째는 무조건 문자이므로, 첫번째 문자이후 숫자일 경우
             if file[i].isdigit():
                 head = file[:i]
                 number = file[i:]
