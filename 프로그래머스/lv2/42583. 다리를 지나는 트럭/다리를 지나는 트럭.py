8308becf95dbb9008288c9e5a6301580cc68f385
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2022-11-03 23:55:00 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -2,11 +2,14 @@ def solution(bridge_length, weight, truck_weights):
     bridge = [0 for _ in range(bridge_length-1)]
     bridge.append(truck_weights.pop(0))
     cnt = 1
+    bridge_sum = bridge[-1]
     while truck_weights:
         cnt += 1
-        bridge.pop(0)
-        if sum(bridge) + truck_weights[0] <= weight:
+
+        bridge_sum -= bridge.pop(0)
+        if bridge_sum + truck_weights[0] <= weight:
             bridge.append(truck_weights.pop(0))
+            bridge_sum += bridge[-1]
         else:
             bridge.append(0)
 
