162e374d5c3dc0a1f7e26adf9e02e5d7a39bdb7b
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2022-11-04 08:36:33 +0900

    [level 2] Title: 2 x n 타일링, Time: 12.50 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.py
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -1,10 +1,8 @@
 def solution(n):
-    if n ==1:
-        return 1
-    if n ==2:
-        return 2
-    arr = [1,2]
-    for _ in range(2,n):
-        arr.append((arr[_-1] + arr[_-2]) % 1000000007)
-    return arr[n-1]
+    arr = [0 for _ in range(n)]
+    arr[0] = 1
+    arr[1] = 2
+    for i in range(2,n):
+        arr[i] = (arr[i-1] + arr[i-2]) % 1000000007
+    return arr[i]
     
