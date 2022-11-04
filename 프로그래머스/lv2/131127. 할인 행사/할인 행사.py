cb85e1b3af077e7eac115e236cdd3cf9d28b8ddc
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2022-11-05 03:01:00 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,24 @@
+
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+from collections import Counter
+def solution(want, number, discount):
+    answer = 0
+    dic = {}
+    for i in range(len(want)):
+        dic[want[i]] = number[i]
+
+    for i in range(len(discount)-9):
+        if dic == Counter(discount[i:i+10]): 
+            answer += 1
+
+    return answer
