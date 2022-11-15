d4109ff55f32e196f80d2ce6e03420918127d291
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2022-11-15 23:04:26 +0900

    [Silver III] Title: 1로 만들기, Time: 604 ms, Memory: 38652 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,15 +1,9 @@
-x = int(input())
-
-arr = [0,0,1,1] + [0] * (x-3)
-
-for i in range(4,x+1):
-    tmp = arr[i-1]
-    if i % 2 == 0:
-        if arr[int(i/2)] < tmp:
-            tmp = arr[int(i/2)]
+n = int(input())
+d = [0] * (n + 1)
+for i in range(2, n + 1):
+    d[i] = d[i - 1] + 1
     if i % 3 == 0:
-        if arr[int(i/3)] < tmp:
-            tmp = arr[int(i/3)]
-    
-    arr[i] = tmp + 1
-print(arr[x])
+        d[i] = min(d[i], d[i // 3] + 1)
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+print(d[n])
