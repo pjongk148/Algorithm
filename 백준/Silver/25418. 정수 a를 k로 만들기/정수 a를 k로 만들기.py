708fd94bdcc643d8ddc9ce2f11975011989e617a
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-03-08 21:51:25 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 104 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
@@ -3,10 +3,8 @@ a, k = map(int,input().split())
 cnt = 0
 while a != k:
     cnt += 1
-    if k % 2 != 0:
-        k -= 1
-    elif k % 2 == 0 and  (k / 2) >= a:
+    if k % 2 == 0 and  (k / 2) >= a:
         k /= 2
-    elif k / 2 < a:
+    else:
         k -= 1
 print(cnt)
