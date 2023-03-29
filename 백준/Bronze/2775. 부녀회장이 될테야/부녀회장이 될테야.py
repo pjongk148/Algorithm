f835c2564320ab193b3f5d5ff52183813de41490
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-03-29 23:51:11 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,10 @@
+t = int(input())
+ 
+for _ in range(t):  
+    floor = int(input())
+    num = int(input())  
+    arr = [x for x in range(1, num+1)]  
+    for _ in range(floor):  
+        for i in range(1, num):
+            arr[i] += arr[i-1]
+    print(arr[-1]) 
