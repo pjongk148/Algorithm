f7f7a7ee8f4a25b4d5d82700acd81d54b8b93f00
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-03-31 14:51:25 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,14 @@
+n = int(input())
+stair = [0]*302
+for i in range(1,n+1):
+    stair[i]=int(input())
+
+arr = [0]*302
+arr[1] = stair[1]
+arr[2] = stair[1]+stair[2]
+arr[3] = max(stair[1]+stair[3], stair[2]+stair[3])
+
+for i in range(4, n+1):
+    arr[i] = max(arr[i-3] + stair[i-1] + stair[i], arr[i-2]+stair[i])
+
+print(arr[n])
