14212084734669baec926e985b999225a8124654
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-04-11 22:53:18 +0900

    [unrated] Title: 숫자 변환하기, Time: 577.75 ms, Memory: 17.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -0,0 +1,18 @@
+def solution(x, y, n):
+    answer = 0
+    DP = [y + 1] * (y+1)
+    DP[x] = 0
+    
+    for i in range(x+1, y+1):
+        if i < x+n and i < 2*x:
+            continue
+            
+        DP[i] = min(DP[i-n]+1, DP[i])
+        
+        if i%2==0:
+            DP[i] = min(DP[i], DP[i//2]+1)
+        
+        if i%3==0:
+            DP[i] = min(DP[i], DP[i//3]+1)
+
+    return DP[y] if DP[y] != (y + 1) else -1
