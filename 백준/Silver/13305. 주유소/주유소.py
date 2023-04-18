c709c383fdcd887e31479fd0c1ab3f46d65840e9
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-04-18 22:06:38 +0900

    [Silver III] Title: 주유소, Time: 144 ms, Memory: 46252 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dist = list(map(int, input().split()))
+price = list(map(int, input().split()))
+
+ans = price[0] * dist[0]
+cur_price = price[0]
+for i in range(1,len(dist)):
+    if price[i] < cur_price:
+        cur_price = price[i]
+    
+    ans += cur_price * dist[i]
+print(ans)
