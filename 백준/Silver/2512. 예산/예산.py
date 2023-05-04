d96ce78087398781da68f4acff03ba9e51209778
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-05-04 23:38:29 +0900

    [Silver III] Title: 예산, Time: 64 ms, Memory: 32276 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.py
@@ -0,0 +1,19 @@
+n = int(input())  
+arr = list(map(int, input().split())) 
+m = int(input())
+
+start, end = 0, max(arr)
+while start <= end:
+    mid = (start + end) // 2 
+    cur = 0
+    for i in arr:
+        if i >= mid:
+            cur += mid
+        else:
+            cur += i 
+    if cur <= m:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
