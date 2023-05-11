837826d6173589c9d4d8bc05ff4e01be76defe22
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-05-11 14:47:59 +0900

    [Silver II] Title: A → B, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,20 +1,15 @@
-x,target = map(int,input().split())
-
-min_cnt = float("inf")
-def dfs(x,target,cnt):
-    global min_cnt
-    if x == target:
-        if cnt < min_cnt:
-            min_cnt = cnt
-            return
-    elif x > target:
-        return
-    else:
-        if target % 2 == 0:
-            dfs(x,target//2,cnt+1)
-        tmp = str(target)
-        if  tmp[-1] == '1':
-            dfs(x,int(tmp[:-1]),cnt+1)
-
-dfs(x,target,1)
-print(min_cnt if min_cnt != float("inf") else -1)
+a,b = map(int,input().split())
+r = 1
+while(b!=a):
+    r+=1
+    temp = b
+    if b%10 == 1:
+        b//=10
+    elif b%2 == 0:
+        b//=2
+    
+    if temp == b:
+        print(-1)
+        break
+else:
+    print(r)
