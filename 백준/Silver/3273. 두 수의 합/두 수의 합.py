daf9e1ad20f56ec560e56da323f12217d0dd6459
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-05-23 23:15:43 +0900

    [Silver III] Title: 두 수의 합, Time: 92 ms, Memory: 43820 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -4,13 +4,12 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 x= int(input())
-nums = dict()
-for i in arr:
-    nums[i] = True
+nums = set(arr)
 
 ans = 0
+
 for i in arr:
-    if (x-i) in nums:
+    if x-i in nums:
         ans += 1
 
 print(ans // 2)
