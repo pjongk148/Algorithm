9f7d4fe9aab9279739eea7139f23229e15633fb1
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-05-31 22:51:55 +0900

    [Silver IV] Title: 베스트셀러, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.py
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.py
@@ -1,13 +1,11 @@
-import sys
-from collections import defaultdict
-input = sys.stdin.readline
-
-books = defaultdict(int)
 n = int(input())
-
-for _ in range(n):
-    book = input().rstrip()
-    books[book] += 1
-
+books = dict()
+for i in range(n):
+    tmp = input().rstrip()
+    if tmp in books:
+        books[tmp] += 1
+    else:
+        books[tmp] = 1
+        
 books = sorted(books.items())
 print(sorted(books, key = lambda x : x[1], reverse = True)[0][0])
