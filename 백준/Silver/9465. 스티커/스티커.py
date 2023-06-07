2bbf60e9cf2794796384f1365c8105f87b71c2ac
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-06-07 15:28:12 +0900

    [Silver I] Title: 스티커, Time: 732 ms, Memory: 40276 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -8,11 +8,15 @@ for i in range(t):
     for _ in range(2):
         arr.append(list(map(int, input().split())))
     
-    for idx in range(1, n):
-        if idx == 1:
-            arr[0][idx] += arr[1][idx - 1]
-            arr[1][idx] += arr[0][idx - 1]
-        else:
-            arr[0][idx] += max(arr[1][idx - 1], arr[1][idx - 2])
-            arr[1][idx] += max(arr[0][idx - 1], arr[0][idx - 2])
+    if n == 1:
+        print(max(max(arr)))
+        continue    
+    
+    arr[0][1] += arr[1][0]
+    arr[1][1] += arr[0][0]
+    
+    for idx in range(2, n):
+        arr[0][idx] += max(arr[1][idx - 1], arr[1][idx - 2])
+        arr[1][idx] += max(arr[0][idx - 1], arr[0][idx - 2])
+        
     print(max(arr[0][n - 1], arr[1][n - 1]))
