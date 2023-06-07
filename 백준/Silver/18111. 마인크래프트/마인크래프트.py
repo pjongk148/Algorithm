61c83832352c6bc23dee94ee1ea79bbf250557e0
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-06-07 11:49:22 +0900

    [Silver II] Title: 마인크래프트, Time: 184 ms, Memory: 34168 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,33 +1,35 @@
+from collections import Counter
 import sys
 input = sys.stdin.readline
 
 n,m,b = map(int, input().split())
+cnt = dict()
 
-arr = [list(map(int,input().rstrip().split())) for _ in range(n)]
-
-min_d = 257
-max_d = 0
-for i in arr:
-    if max_d < max(i):
-        max_d = max(i)
-    if min_d > min(i):
-        min_d = min(i)
-
+for _ in range(n):
+    tmp = Counter(map(int,input().rstrip().split()))
+    for idx in tmp.keys():
+        if idx in cnt:
+            cnt[idx] += tmp[idx]
+        else:
+            cnt[idx] = tmp[idx]
+            
+min_d = min(cnt.keys())
+max_d = max(cnt.keys())
 depth = [i for i in range(min_d,max_d+1)]
 min_time = float("inf")
 ans = 0
+
 for target in depth:
     tmp_time = 0
     tmp_block = b
-    for i in range(n):
-        for j in range(m):
-            dif = arr[i][j] - target
-            if dif > 0 :
-                tmp_block += dif
-                tmp_time += dif * 2
-            elif dif < 0:
-                tmp_block += dif
-                tmp_time -= dif
+    for i in cnt.keys():
+        dif = i - target
+        if dif > 0 :
+            tmp_block += dif * cnt[i]
+            tmp_time += dif * 2 * cnt[i]
+        elif dif < 0:
+            tmp_block += dif * cnt[i]
+            tmp_time -= dif * cnt[i]
                 
     if tmp_block < 0:
         continue      
