a180aab18b3bad4bc1ddf2c9e8e58b6ec431b3fe
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-06-08 16:11:08 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,16 +1,11 @@
 n = int(input())
+
 cnt = 0
- 
-while True:
-    if n % 5 != 0:
-        n = n - 3
-        cnt += 1
- 
-    elif n % 5 == 0:
+while n > 0:
+    if n % 5 == 0:
         cnt += n // 5
-        print(cnt)
-        break
- 
-    if n < 0:
-        print(-1)
-        break
+        n = 0
+    else:
+        n -= 3
+        cnt += 1
+print(-1) if n < 0 else print(cnt)
