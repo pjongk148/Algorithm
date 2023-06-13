29e1dccfc500c14a3886646b0dad2f8871b31a04
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-06-13 11:34:36 +0900

    [Silver II] Title: 좌표 압축, Time: 2092 ms, Memory: 154820 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,20 +1,16 @@
 import sys
 input = sys.stdin.readline
-
 n = int(input())
-
 arr = list(map(int, input().split()))
 
-sorted_arr= sorted(arr)
-new_arr = dict()
-new_arr[sorted_arr[0]] = 0
-start = 0
-for i in range(1,n):
-    if sorted_arr[i-1] < sorted_arr[i]: 
-        start += 1
-    new_arr[sorted_arr[i]] = start
+sorted_arr = sorted(list(set(arr)))
+val = dict()
+idx = 0
+for i in sorted_arr:
+    val[i] = idx
+    idx += 1
 
-new_arr
-
-for i in range(n):
-    print(new_arr[arr[i]], end =" ")
+for i in range(len(arr)-1):
+    print(val[arr[i]],end=" ")
+    
+print(val[arr[-1]])
