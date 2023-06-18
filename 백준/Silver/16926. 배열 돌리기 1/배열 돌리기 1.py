3c4139998b4c198e3b0158f5d56f0e027e1cc783
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-06-18 17:18:35 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 4036 ms, Memory: 36464 KB -BaekjoonHub

---
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -0,0 +1,41 @@
+import sys
+import math
+input = sys.stdin.readline
+
+n,m,r = map(int, input().split())
+
+arr =[list(map(int, input().split())) for _ in range(n)]
+
+def rotate(x,y,z,w):
+    if x* 2 >= z or y * 2>= w:
+        return
+    
+    tmp = arr[z-x][y]
+    
+    for i in range(z-x-1,x-1,-1):
+        arr[i+1][y] = arr[i][y]
+    
+    tmp2 = arr[z-x][w-y]
+
+    arr[z-x][y+1:w-y+1] = [tmp] + arr[z-x][y+1:w-y]
+
+    tmp3 = arr[x][w-y]
+    
+    for i in range(x,z-x):
+        arr[i][w-y] = arr[i+1][w-y]
+  
+    arr[z-(x+1)][w-y] = tmp2
+    
+    arr[x][x:w-y] = arr[x][y+1:w-y] + [tmp3]
+    x += 1
+    y += 1
+    
+    rotate(x,y,z,w)
+    
+for i in range(r):
+    rotate(0,0,len(arr)-1,len(arr[0])-1)
+
+for i in range(n):
+    for j in range(m):
+        print(arr[i][j], end= " ")
+    print()   
