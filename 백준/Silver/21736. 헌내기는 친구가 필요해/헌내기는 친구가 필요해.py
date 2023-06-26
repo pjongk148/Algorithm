8eee1212d6357c12a901d8da85753ddd955f92d1
Jonyuuuk <77969043+pjongk148@users.noreply.github.com>
2023-06-26 16:37:30 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 408 ms, Memory: 39520 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,43 +1,34 @@
-import sys
-input = sys.stdin.readline
+from collections import deque
 
-n,m = map(int,input().split())
-arr = []
-visited = [[False] * m for _ in range(n)]
-x = -1
-y = -1
+n, m = map(int, input().split()) # 캠퍼스 크기 입력 받음
+map_ = [list(input()) for _ in range(n)] # 캠퍼스 정보 입력 받음
+visited = [[0]*m for _ in range(n)] # 방문 확인을 위한 변수 선언
+
+def bfs(x, y):
+    cnt = 0 # 방문 횟수
+    q = deque() # 큐 선언
+    visited[x][y] = 1 # 도연이가 있는 위치를 방문 처리
+    q.append((x, y)) # 큐에 추가
+  
+    while q:
+        x, y = q.popleft()
+        for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            # 도연이 이동 좌표
+            nx, ny = x + dx, y + dy 
+
+          # 도연이 이동 좌표가 맵안에서 처음 방문한곳을 체크
+            if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0:
+                if map_[nx][ny] != 'X':
+                    q.append((nx, ny))
+                    visited[nx][ny] = 1
+                    if map_[nx][ny] == 'P':
+                        cnt += 1
+    return cnt                        
 for i in range(n):
-    arr.append(list(input()))
-    if "I" in arr[i]:
-        x = i
-        y = arr[i].index("I")
-
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-
-queue = [[x,y]]
-ans = 0
-
-while queue:
-    x,y = queue.pop()
-    
-        
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if nx < 0 or ny < 0 or nx >= n or ny >= m:
-            continue
-            
-        else:
-            if not visited[nx][ny]:
-
-                if arr[nx][ny] == "P":
-                    ans += 1
-                    visited[nx][ny] = True
-                    queue.append([nx,ny])
-
-                elif arr[nx][ny] == "O":
-                    visited[nx][ny] = True
-                    queue.append([nx,ny])
-
-print(ans) if ans > 0 else print("TT")
+    for j in range(m):
+        if map_[i][j] == 'I':
+            cnt = bfs(i, j)
+if cnt != 0:
+    print(cnt)
+else:
+    print('TT')
